3a554e7567e0eeb2501b5fd847d3f30003129bf5
Oguz <ouzdemirtas@hotmail.com>
2019-05-19 14:13:36 +0300

    Add files via upload

---
+++ wakeupneo2.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+clear
+sleep 5
+echo -n "W"
+sleep 0.1
+echo -n "a"
+sleep 0.1
+echo -n "k"
+sleep 0.1
+echo -n "e"
+sleep 0.1
+echo -n " "
+sleep 0.1
+echo -n "u"
+sleep 0.1
+echo -n "p"
+sleep 0.1
+echo -n ", "
+sleep 0.1
+echo -n "N"
+sleep 0.1
+echo -n "e"
+sleep 0.1
+echo -n "o"
+sleep 0.1
+echo -n "."
+sleep 0.1
+echo -n "."
+sleep 0.1
+echo -n "."
+sleep 5
+clear
+sleep 1
+echo -n "T"
+sleep 0.7
+echo -n "h"
+sleep 0.7
+echo -n "e"
+sleep 0.7
+echo -n " "
+sleep 0.4
+echo -n "M"
+sleep 0.1
+echo -n "a"
+sleep 0.1
+echo -n "t"
+sleep 0.1
+echo -n "r"
+sleep 0.1
+echo -n "i"
+sleep 0.1
+echo -n "x"
+sleep 0.1
+echo -n " "
+sleep 0.1
+echo -n "h"
+sleep 0.1
+echo -n "a"
+sleep 0.1
+echo -n "s"
+sleep 0.1
+echo -n " "
+sleep 0.1
+echo -n "y"
+sleep 0.1
+echo -n "o"
+sleep 0.1
+echo -n "u"
+sleep 0.1
+echo -n "."
+sleep 0.1
+echo -n "."
+sleep 0.1
+echo -n "."
+sleep 5
+clear
+sleep 1
+echo -n "F"
+sleep 0.1
+echo -n "o"
+sleep 0.1
+echo -n "l"
+sleep 0.1
+echo -n "l"
+sleep 0.1
+echo -n "o"
+sleep 0.1
+echo -n "w"
+sleep 0.1
+echo -n " "
+sleep 0.1
+echo -n "t"
+sleep 0.1
+echo -n "h"
+sleep 0.1
+echo -n "e"
+sleep 0.1
+echo -n " "
+sleep 0.1
+echo -n "w"
+sleep 0.1
+echo -n "h"
+sleep 0.1
+echo -n "i"
+sleep 0.1
+echo -n "t"
+sleep 0.1
+echo -n "e"
+sleep 0.1
+echo -n " "
+sleep 0.1
+echo -n "r"
+sleep 0.1
+echo -n "a"
+sleep 0.1
+echo -n "b"
+sleep 0.1
+echo -n "b"
+sleep 0.1
+echo -n "i"
+sleep 0.1
+echo -n "t."
+sleep 4
+echo
+clear
+echo -n "Knock, knock, Neo."
+sleep 4
+echo
